1cc55c3b4838216a5139df68fe2f87b1b219af7e
tetraloba <four.leaves.siva7342@gmail.com>
2025-07-21 08:59:24 +0900

    debug_log_arround_main_warn

--- source/exe/main.cc
+++ source/exe/main.cc
@@ -14,7 +14,11 @@
  * after setting up command line options.
  */
 int main(int argc, char** argv) {
-  ENVOY_LOG(info, "tetraloba: main.cc:63 main() called!");
+  ENVOY_LOG(info, "tetraloba: main.cc:63 main() called! info");
+  ENVOY_LOG(warn, "tetraloba: main.cc:63 main() called! warn");
+  ENVOY_LOG(error, "tetraloba: main.cc:63 main() called! error");
+  cout << "tetraloba: main.cc:63 main() called! stdout" << endl;
+  cerr << "tetraloba: main.cc:63 main() called! stderr" << endl;
 #ifdef WIN32
   Envoy::ServiceBase service;
   if (!Envoy::ServiceBase::TryRunAsService(service)) {
